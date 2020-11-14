b31afd0e4a1cb536187415860161260fbd2a5b37
Eric Liu <ericliu4943@gmail.com>
2020-11-13 21:37:25 -0800

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Eric Liu on 11/13/20.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
