b10aa62794c672b2f508e7d5c2ac828ba22ffcff
Eric Liu <ericliu4943@gmail.com>
2020-11-16 23:47:40 -0800

    Image selectable and shows up on selection screen

--- Instafilter/ImagePicker.swift
+++ Instafilter/ImagePicker.swift
@@ -2,44 +2,42 @@
 //  ImagePicker.swift
 //  Instafilter
 //
-//  Created by Eric Liu on 11/14/20.
+//  Created by Eric Liu on 11/16/20.
 //
 
-import Foundation
 import SwiftUI
 
 struct ImagePicker: UIViewControllerRepresentable {
-  @Binding var image: UIImage?
-  @Environment(\.presentationMode) var presentationMode
-  
-  class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    var parent: ImagePicker
+  class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    let parent: ImagePicker
+    
     init(_ parent: ImagePicker) {
       self.parent = parent
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-
       if let uiImage = info[.originalImage] as? UIImage {
         parent.image = uiImage
       }
-
-      print("\(parent.presentationMode.wrappedValue)")
+      
       parent.presentationMode.wrappedValue.dismiss()
     }
   }
+
+  @Environment(\.presentationMode) var presentationMode
+  @Binding var image: UIImage?
   
   func makeCoordinator() -> Coordinator {
     Coordinator(self)
   }
   
-  func makeUIViewController(context: Context) -> UIImagePickerController {
+  func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
     let picker = UIImagePickerController()
     picker.delegate = context.coordinator
     return picker
   }
   
-  func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+  func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
     
   }
 }
