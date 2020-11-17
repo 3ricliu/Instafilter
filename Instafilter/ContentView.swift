42f05c1d7b3ac84e2720ecb08e396b0e1cfa9bfc
Eric Liu <ericliu4943@gmail.com>
2020-11-17 00:32:19 -0800

    Selected photo can be adjusted by sepia filter

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,9 +5,9 @@
 //  Created by Eric Liu on 11/13/20.
 //
 
+import SwiftUI
 import CoreImage
 import CoreImage.CIFilterBuiltins
-import SwiftUI
 
 
 struct ContentView: View {
@@ -16,8 +16,20 @@ struct ContentView: View {
   @State private var showingImagePicker = false
   @State private var inputImage: UIImage?
 
+  @State var currentFilter = CIFilter.sepiaTone()
+  let context = CIContext()
+
   var body: some View {
-    NavigationView {
+    let intensity = Binding<Double>(
+      get: {
+        self.filterIntensity
+      },
+      set: {
+        self.filterIntensity = $0
+        self.applyProcessing()
+      }
+    )
+    return NavigationView {
       VStack {
         ZStack {
           Rectangle()
@@ -39,7 +51,7 @@ struct ContentView: View {
         
         HStack {
           Text("Intensity")
-          Slider(value: self.$filterIntensity)
+          Slider(value: intensity)
         }.padding(.vertical)
         
         HStack {
@@ -64,7 +76,20 @@ struct ContentView: View {
   
   func loadImage() {
     guard let inputImage = inputImage else { return }
-    image = Image(uiImage: inputImage)
+    let beginImage = CIImage(image: inputImage)
+    currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+    applyProcessing()
+  }
+  
+  func applyProcessing() {
+    currentFilter.intensity = Float(filterIntensity)
+    guard let outputImage = currentFilter.outputImage
+    else { return }
+    
+    if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+      let uiImage = UIImage(cgImage: cgimg)
+      image = Image(uiImage: uiImage)
+    }
   }
 }
 
